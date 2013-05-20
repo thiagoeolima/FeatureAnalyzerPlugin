f4a28bf5d8f584072f2de88025ebe975c8b525fd
thiagoeolima <thiagorms18@gmail.com>
2013-05-20 18:32:37 -0300

    FSTModel and Feature Merkers fixed

--- branches/prerelease/FeatureAnalyzerPlugin/src/br/ufal/ic/featureanalyzer/core/CPPComposer.java
+++ branches/prerelease/FeatureAnalyzerPlugin/src/br/ufal/ic/featureanalyzer/core/CPPComposer.java
@@ -7,6 +7,7 @@ import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
+import java.util.Stack;
 import java.util.Vector;
 import java.util.regex.Pattern;
 
@@ -17,8 +18,15 @@ import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceDelta;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Display;
+import org.prop4j.And;
+import org.prop4j.Node;
+import org.prop4j.Not;
 
 import br.ufal.ic.featureanalyzer.activator.FeatureAnalyzer;
 import br.ufal.ic.featureanalyzer.controllers.ProjectExplorerController;
@@ -66,6 +74,7 @@ public class CPPComposer extends PPComposerExtensionClass {
 		// Setup the controller
 		typeChef = new TypeChef();
 		prepareFullBuild(null);
+		annotationChecking();
 
 		if (supSuccess == false || cppModelBuilder == null) {
 			return false;
@@ -149,8 +158,10 @@ public class CPPComposer extends PPComposerExtensionClass {
 		//
 		//
 		//
-		if (cppModelBuilder != null)
+		if (cppModelBuilder != null) {
 			cppModelBuilder.buildModel();
+		}
+		annotationChecking();
 	}
 
 	/*
@@ -161,9 +172,97 @@ public class CPPComposer extends PPComposerExtensionClass {
 	 */
 	@Override
 	public void postModelChanged() {
-		deleteAllPreprocessorAnotationMarkers();
 		prepareFullBuild(null);
-		// annotationChecking();
+		annotationChecking();
+	}
+
+	private void annotationChecking() {
+		deleteAllPreprocessorAnotationMarkers();
+		Job job = new Job("preprocessor annotation checking") {
+			@Override
+			protected IStatus run(IProgressMonitor monitor) {
+				annotationChecking(featureProject.getSourceFolder());
+				return Status.OK_STATUS;
+			}
+		};
+		job.setPriority(Job.SHORT);
+		job.schedule();
+	}
+
+	private void annotationChecking(IFolder folder) {
+		try {
+			for (final IResource res : folder.members()) {
+				if (res instanceof IFolder) {
+					annotationChecking((IFolder) res);
+				} else if (res instanceof IFile) {
+					final Vector<String> lines = loadStringsFromFile((IFile) res);
+					// do checking and some stuff
+					processLinesOfFile(lines, (IFile) res);
+				}
+			}
+		} catch (CoreException e) {
+			FeatureAnalyzer.getDefault().logError(e);
+		}
+	}
+
+	synchronized private void processLinesOfFile(Vector<String> lines, IFile res) {
+		expressionStack = new Stack<Node>();
+
+		// count of if, ifelse and else to remove after processing of else from
+		// stack
+		ifelseCountStack = new Stack<Integer>();
+
+		// go line for line
+		for (int j = 0; j < lines.size(); ++j) {
+			String line = lines.get(j);
+
+			// if line is preprocessor directive
+			if (line.contains("#")) {
+				if (line.contains("#if ") || line.contains("#elif ")
+						|| line.contains("#ifdef ")
+						|| line.contains("#ifndef ") || line.contains("#else")) {
+
+					// remove defined directive to proper work as normal
+					// preprocessor.
+					line = line.replaceAll("defined", "");
+
+					// if e1, elseif e2, ..., elseif en == if -e1 && -e2 && ...
+					// && en
+					// if e1, elseif e2, ..., else == if -e1 && -e2 && ...
+					if (line.contains("#elif ") || line.contains("#else")) {
+						if (!expressionStack.isEmpty()) {
+							Node lastElement = new Not(expressionStack.pop()
+									.clone());
+							expressionStack.push(lastElement);
+						}
+					} else if (line.contains("#if ")
+							|| line.contains("#ifdef ")
+							|| line.contains("#ifndef ")) {
+						ifelseCountStack.push(0);
+					}
+
+					if (!ifelseCountStack.empty() && !line.contains("#else"))
+						ifelseCountStack.push(ifelseCountStack.pop() + 1);
+
+					setMarkersContradictionalFeatures(line, res, j + 1);
+
+					setMarkersNotConcreteFeatures(line, res, j + 1);
+				} else if (line.contains("#endif")) {
+					while (!ifelseCountStack.empty()) {
+						if (ifelseCountStack.peek() == 0)
+							break;
+
+						if (!expressionStack.isEmpty())
+							expressionStack.pop();
+
+						ifelseCountStack.push(ifelseCountStack.pop() - 1);
+					}
+
+					if (!ifelseCountStack.empty())
+						ifelseCountStack.pop();
+				}
+			}
+		}
 	}
 
 	/**
@@ -186,6 +285,68 @@ public class CPPComposer extends PPComposerExtensionClass {
 						lineNumber, res);
 			}
 		}
+	}
+
+	/**
+	 * Checks given line if it contains expressions which are always
+	 * <code>true</code> or <code>false</code>.<br />
+	 * <br />
+	 * 
+	 * Check in three steps:
+	 * <ol>
+	 * <li>just the given line</li>
+	 * <li>the given line and the feature model</li>
+	 * <li>the given line, the surrounding lines and the feature model</li>
+	 * </ol>
+	 * 
+	 * @param line
+	 *            content of line
+	 * @param res
+	 *            file containing given line
+	 * @param lineNumber
+	 *            line number of given line
+	 */
+	private void setMarkersContradictionalFeatures(String line, IFile res,
+			int lineNumber) {
+		if (line.contains("#else")) {
+			if (!expressionStack.isEmpty()) {
+				Node[] nestedExpressions = new Node[expressionStack.size()];
+				nestedExpressions = expressionStack.toArray(nestedExpressions);
+
+				And nestedExpressionsAnd = new And(nestedExpressions);
+
+				isContradictionOrTautology(nestedExpressionsAnd.clone(), true,
+						lineNumber, res);
+			}
+
+			return;
+		}
+
+		boolean negative = line.contains("#ifndef ");
+
+		// remove "//#if ", "//ifdef", ...
+		// TODO TIRAR O defined
+		line = replaceCommandPattern.matcher(line).replaceAll("");
+
+		// prepare expression for NodeReader()
+		line = line.trim();
+		line = line.replace("&&", "&");
+		line = line.replace("||", "|");
+		line = line.replace("!", "-");
+		line = line.replace("&", " and ");
+		line = line.replace("|", " or ");
+		line = line.replace("-", " not ");
+
+		// get all features and generate Node expression for given line
+		Node ppExpression = nodereader.stringToNode(line, featureList);
+
+		if (ppExpression != null) {
+			if (negative)
+				ppExpression = new Not(ppExpression.clone());
+
+			doThreeStepExpressionCheck(ppExpression, lineNumber, res);
+		}
+
 	}
 
 	/**
@@ -266,8 +427,8 @@ public class CPPComposer extends PPComposerExtensionClass {
 			public void run() {
 				AnalyzerViewController viewController = AnalyzerViewController
 						.getInstance();
-				viewController.showView();
 				if (typeChef.getLogs().length > 0) {
+					viewController.showView();
 					viewController.adaptTo(typeChef.getLogs());
 					continueCompilationFlag = MessageDialog.openQuestion(
 							display.getActiveShell(),
@@ -415,9 +576,8 @@ public class CPPComposer extends PPComposerExtensionClass {
 
 					if (!ProjectConfigurationErrorLogger.getInstance()
 							.getProjectsList().isEmpty()) {
-						
 						invalidProductViewController.showView();
-						
+
 						List<InvalidProductViewLog> logs = new LinkedList<InvalidProductViewLog>();
 						for (String s : ProjectConfigurationErrorLogger
 								.getInstance().getProjectsList()) {
